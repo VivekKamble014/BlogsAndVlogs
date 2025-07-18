207dafe2705c69e95dc859ef5c566fb48f20fe3b
Vivek Kamble <mrvivekkamble8@gmail.com>
2025-07-18 13:55:35 +0530

    froentend done

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,26 @@
+// import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:5001/api' ,
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const auth = JSON.parse(localStorage.getItem('auth'));
+//     if (auth?.token) {
+//       config.headers.Authorization = `Bearer ${auth.token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://blogsandvlogsbackend.onrender.com/api', // ✅ Updated to production URL
 });
 
 api.interceptors.request.use(
